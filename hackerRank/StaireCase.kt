77944324fe73188479687d3379bb8c8f7e2fa65c
sumeyraozugur <ozugursumeyra@gmail.com>
2023-02-10 01:13:02 +0300

    staire case🍇

---
+++ hackerRank/StaireCase.kt
@@ -0,0 +1,21 @@
+
+fun main(){
+    stairCase(6)
+}
+/*
+      #
+     ##
+    ###
+   ####
+  #####
+ ######
+
+*/
+
+fun stairCase(n: Int) {
+    for (i in 1..n) {
+        println(" ".repeat(n - i) + "#".repeat(i))
+    }
+}
+
+
