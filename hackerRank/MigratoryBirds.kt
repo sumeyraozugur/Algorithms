d7ca962940ed6c48dc797cdd951b73314a6c6c4b
sumeyraozugur <ozugursumeyra@gmail.com>
2023-02-10 17:38:29 +0300

    migratory birds🥑

---
+++ hackerRank/MigratoryBirds.kt
@@ -0,0 +1,21 @@
+fun main(){
+    println(migratoryBirds(arrayOf(1, 4, 4, 4, 5, 3)))
+}
+
+fun migratoryBirds(arr: Array<Int>): Int {
+    val frequencyMap = HashMap<Int, Int>()
+
+    for (element in arr) {
+        if (frequencyMap.containsKey(element)) {
+            frequencyMap[element] = frequencyMap[element]!! + 1
+        } else {
+            frequencyMap[element] = 1
+        }
+    }
+
+    val maxFrequency = frequencyMap.values.max()
+
+    val mostCommonElements = frequencyMap.filterValues { it == maxFrequency }.keys
+
+    return mostCommonElements.first()
+}
