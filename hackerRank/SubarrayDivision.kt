7ba63b7ad2922705e245dd6e1c07307ada53b4f5
sumeyraozugur <ozugursumeyra@gmail.com>
2023-02-10 14:59:22 +0300

    subarray division💦

---
+++ hackerRank/SubarrayDivision.kt
@@ -0,0 +1,15 @@
+
+fun main(){
+    birthday(arrayOf(1, 2, 1, 3, 2),3,2)
+}
+
+fun birthday(s: Array<Int>, d: Int, m: Int): Int {
+
+    var counter = 0
+    for(start in 0..s.size-m){
+        if(s.copyOfRange(start, start+m).sum() ==d){
+            counter +=1
+        }
+    }
+    return counter
+}
