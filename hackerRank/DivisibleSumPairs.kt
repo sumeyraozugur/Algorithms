8a3786cf4110d0d32a5a4aa020b6fbb079ecab83
sumeyraozugur <ozugursumeyra@gmail.com>
2023-02-10 15:13:09 +0300

    divisible sum pairs

---
+++ hackerRank/DivisibleSumPairs.kt
@@ -0,0 +1,13 @@
+fun main(){
+    println(divisibleSumPairs(6,3, arrayOf(1, 3, 2, 6, 1, 2)))
+}
+
+fun divisibleSumPairs(n: Int, k: Int, ar: Array<Int>): Int {
+    var counter =0
+    for(i in 0 until n){
+        for(j in i+1 until n){
+            if((ar[i] +ar[j]) % k ==0) counter ++
+        }
+    }
+    return  counter
+}
