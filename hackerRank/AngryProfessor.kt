805ff4b575dbb0c0e5ea9f7fe837a410e70247c9
sumeyraozugur <ozugursumeyra@gmail.com>
2023-02-15 00:28:01 +0300

    Angry professor😈

---
+++ hackerRank/AngryProfessor.kt
@@ -0,0 +1,9 @@
+fun main(){
+    println( angryProfessor(3, arrayOf(-2,-1,0,1,2)))
+
+}
+fun angryProfessor(k: Int, a: Array<Int>): String {
+    val continueLesson = a.count { students -> students <=0 }
+    return if (continueLesson >=k) "NO" //Cancelled no
+    else  "YES"
+}
